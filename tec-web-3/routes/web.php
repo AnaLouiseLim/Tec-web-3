e92013c8bdd93b2901b2d1677bc786675d9d2629
Ana Louise de Lima Silva <analouiselim@gmail.com>
2026-04-10 00:12:02 +0000

    atualização

--- tec-web-3/routes/web.php
+++ tec-web-3/routes/web.php
@@ -14,4 +14,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
+Route::resource('tec-web-3', App\Http\Controllers\Atividade2::class);
+
 require __DIR__.'/settings.php';
